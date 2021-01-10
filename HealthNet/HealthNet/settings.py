c5b542bf7eef248d61aa861d5b0749988fb1ab0a
Faizsyed98 <72322384+Faizsyed98@users.noreply.github.com>
2021-01-10 22:20:51 +0530

    Update settings.py

--- HealthNet/HealthNet/settings.py
+++ HealthNet/HealthNet/settings.py
@@ -14,8 +14,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x_p!s9fcx+ra!wb-%kmlr*c^=q%6*4pq#7lldts75_1j-$mp4l'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
